0a4e5393fa3ba0af895db1e89f7ed7af92f30084
SewerKomp <seweryn.mlodawski@gmail.com>
2025-10-15 21:30:15 +0200

    Close work

--- 4_funkcje/funkcje_3.py
+++ 4_funkcje/funkcje_3.py
@@ -4,3 +4,22 @@ def day_times():
 times = day_times()
 print(times)
 print(type(times))
+
+first, second, third, fourth = day_times()
+print("Trzeci element to %s" % third)
+
+#---------------
+# funkcja i jej kolejne wywołania
+def add(a, b):
+    print(a + b)
+
+add(2, 2)
+add(54, 64)
+add(40504, 232320)
+
+#------------
+
+def customized_hello(first_name, last_name):
+    print("Hello Mr %s %s" % (first_name, last_name))
+
+customized_hello("John", "Cleese")
