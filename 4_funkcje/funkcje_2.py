a48e0230362fe831bb9b2657fd5d9ab015dddba2
SewerKomp <seweryn.mlodawski@gmail.com>
2025-10-15 20:36:19 +0200

    Add file funkcje_3

--- 4_funkcje/funkcje_2.py
+++ 4_funkcje/funkcje_2.py
@@ -1,1 +1,15 @@
 #one two fri
+def shopping():
+    shopping_items = [
+        "jajka",
+        "bułka",
+        "ser feta",
+        "masło",
+        "pomidor"
+    ]
+    shopping_cart = "Koszyk zawiera: "
+    for item in shopping_items:
+        shopping_cart += item + '\n'
+    return shopping_cart
+
+print(shopping())
---
+++ 4_funkcje/funkcje_3.py
@@ -0,0 +1,6 @@
+def day_times():
+    return "morning", "afternoon", "evening", "night"
+
+times = day_times()
+print(times)
+print(type(times))
