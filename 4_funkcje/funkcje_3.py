5a17651121de40ce2ea2fdaa3e0d2d657625d085
SewerKomp <seweryn.mlodawski@gmail.com>
2025-10-16 19:07:08 +0200

    Add new file basket

--- 4_funkcje/funkcje_3.py
+++ 4_funkcje/funkcje_3.py
@@ -23,3 +23,11 @@ def customized_hello(first_name, last_name):
     print("Hello Mr %s %s" % (first_name, last_name))
 
 customized_hello("John", "Cleese")
+
+#------------------
+print(f"\n gender prefix\n")
+
+def customized_hello(first_name, last_name, gender_prefix='Mr'):
+    print(f"Hello {gender_prefix} {first_name} {last_name}!")
+
+customized_hello("Anna", "Maria", "Pani") # jesli tu nie podam gender prefiksu - to podepnie domyślny czyli Mr
---
+++ 4_funkcje/funkcje_4.py
@@ -0,0 +1,9 @@
+shopping_items = [
+    "jajka",
+    "bułka",
+    "ser feta",
+    "masło",
+    "pomidor"
+]
+
+def shopp
